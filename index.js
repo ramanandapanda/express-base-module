5df75a9fde97a22b156e2a6bb779543c7ae4984d
Ramananda Panda <ramanandapanda@hotmail.com>
2024-01-31 15:06:15 +0530

    users submodule

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const users = require("./express-users-submodule/users");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-
+app.use(users);
 app.listen(3000);
